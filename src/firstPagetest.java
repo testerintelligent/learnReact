be2f8bddd2605450b5ab4d41f512005c8e256540
testerintelligent <45865059+testerintelligent@users.noreply.github.com>
2019-01-21 21:17:04 +0530

    First

---
+++ src/firstPagetest.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Admin_Guru on 21-01-2019.
+ */
+public class firstPagetest {
+    public static void main(String args[])
+    {
+        System.out.println ("\n Hello World");
+        System.out.println ("\n Hello Tamil");
+
+
+        final String currentTime    = String.valueOf(System.currentTimeMillis());
+
+
+        System.out.println (currentTime);
+
+    }
+
+
+
+}
