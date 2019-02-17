d61b55707dc075e837ac7dc315cc27c420273ae4
testerintelligent <45865059+testerintelligent@users.noreply.github.com>
2019-02-17 13:46:10 +0530

    Third

--- src/firstPagetest.java
+++ src/firstPagetest.java
@@ -1,18 +1,16 @@
 /**
  * Created by Admin_Guru on 21-01-2019.
  */
+
+import java.time.LocalDateTime;
+
 public class firstPagetest {
     public static void main(String args[])
     {
-        System.out.println ("\n Hello World");
-        System.out.println ("\n Hello Tamil1");
-
-
-        final String currentTime    = String.valueOf(System.currentTimeMillis());
-
-
-        System.out.println (currentTime);
-
+        System.out.println ("Hello World \n");
+        System.out.println ("Hello Tamil \n");
+        LocalDateTime current = LocalDateTime.now();
+        System.out.println("Current Date and Time is: " + current);
     }
 
 
